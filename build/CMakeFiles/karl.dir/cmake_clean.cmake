e028830a1af713e3fb3a6ffb28b922cc3ac04d2e
创造君 <chuangzaojun@126.com>
2022-11-26 10:45:00 +0800

    update something about CMake

---
+++ build/CMakeFiles/karl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/karl.dir/main.cpp.obj"
+  "CMakeFiles/karl.dir/main.cpp.obj.d"
+  "karl.exe"
+  "karl.exe.manifest"
+  "karl.pdb"
+  "libkarl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/karl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/karl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for karl.
+# This may be replaced when dependencies are built.
